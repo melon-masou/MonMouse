2ea078635c2422e0765f1f9296e5b392263cd933
melon.masou <melon.masou@outlook.com>
2024-10-13 18:36:17 +0800

    add check+release workflow

--- build.rs
+++ build.rs
@@ -63,10 +63,15 @@ fn windows_rc_compile(content: String, out_dir: &str, rc_file: &str, lib_file: &
 }
 
 fn main() {
-    println!(
-        "cargo:rustc-env=VERSION_ANNO={}",
-        env::var("VERSION_ANNO").unwrap_or("dev".to_string())
-    );
+    let anno = match env::var("VERSION_ANNO") {
+        Ok(st) => match st.as_str() {
+            "release" => "",
+            "" => "dev",
+            v => &v.to_owned(),
+        },
+        Err(_) => "dev",
+    };
+    println!("cargo:rustc-env=VERSION_ANNO={}", anno);
     let sha = env::var("VERSION_SHA").unwrap_or_default();
     println!(
         "cargo:rustc-env=VERSION_SHA={}",
